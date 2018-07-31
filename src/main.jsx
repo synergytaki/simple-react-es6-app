78bdf9089e164690f345dae5164bb0dbfb39a083
taki.akinori <taki.akinori@synergy101.jp>
2018-07-31 17:07:53 +0900

    ライブラリとの連携

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import Hello from 'simple-react-es6';
+import Hello from 'simple-react-es6/core/Hello';
+import Hello2 from 'simple-react-es6/core/Hello2';
+// import {Hello, Hello2} from 'simple-react-es6/core';
+
 import World from './components/world.jsx';
+import Button from '@material-ui/core/Button';
 
 render(
   <div>
     <Hello />
+    <Hello2 />
     <World />
+    <Button variant="raised" color="primary">
+      Hello World
+    </Button>
   </div>,
   document.getElementById('app')
 );
